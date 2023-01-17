c083d7b594f45e3e1bc278f424443208e22008a5
Logan Finley <finley4.logan@gmail.com>
2023-01-16 21:15:04 -0800

    Added ball movement/collision

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -1,20 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
-    public Rigidbody2D myRigidBody;
-    public float ballSpeed;
+    private Rigidbody2D rb2d;
+    private Vector2 direction;
 
-    // Start is called before the first frame update
+    public float ballSpeed = 200.0f;
+
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        // determine whether the ball goes to the left or the right
+        if (Random.value < 0.5f) {
+            direction.x = -1.0f;
+        }
+        else {
+            direction.x = 1.0f;
+        }
+
+        // determine the value of the y component of the ball's inital velocity
+        if (Random.value < 0.5f) {
+            direction.y = Random.Range(-1.0f, -0.5f);
+        }
+        else {
+            direction.y = Random.Range(0.5f, 1.0f);
+        }
+
+        //rb2d.velocity = direction * ballSpeed;
+        rb2d.AddForce(direction * ballSpeed);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        myRigidBody.velocity = Vector2.left * ballSpeed;
     }
 }
